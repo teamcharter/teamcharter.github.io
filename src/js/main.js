d8d3c28d96d9805788d967df60104ed3a5669697
Vinesh Kannan <vinesh@omnipointment.com>
2017-08-05 18:07:28 -0500

    Add progress updates tab

--- src/js/main.js
+++ src/js/main.js
@@ -11,7 +11,10 @@ let views = Views();
 
 let loginBtn = document.getElementById('login');
 let feedbackBtn = document.getElementById('feedback');
+
 let inviteBtn = document.getElementById('invite');
+let tabList = document.getElementsByClassName('charter-tab');
+
 let teamName = document.getElementById('team-name');
 let teamQuest = document.getElementById('team-question');
 let saveQuest = document.getElementById('save-question');
@@ -20,6 +23,9 @@ let saveExpect = document.getElementById('save-expectations');
 let myUpdate = document.getElementById('my-update');
 let saveUpdate = document.getElementById('save-update');
 let teamUpdates = document.getElementById('team-updates');
+
+let progressUpdates = document.getElementById('progress-updates');
+//let charterUpdates = document.getElementById('charter-updates');
 
 loginBtn.addEventListener('click', (e) => {
 	database.login(main);
@@ -39,6 +45,15 @@ function main(user) {
 		fillText('fill-user-name', user.displayName);
 		fillSrc('fill-user-image', user.photoURL);
 
+		for (let k = 0; k < tabList.length; k++) {
+			tabList[k].addEventListener('click', (e) => {
+				onTabClick(tabList, tabList[k]);
+			});
+		}
+
+		// Show the Charter tab when the page loads
+		onTabClick(tabList, document.querySelectorAll('.charter-tab[data-tab="container-charter"]')[0]);
+
 		let prometheus = database.getPrometheus();
 
 		feedbackBtn.addEventListener('click', (e) => {
@@ -55,123 +70,166 @@ function main(user) {
 			});
 		});
 
-		inviteBtn.addEventListener('click', (e) => {
-			database.getTeam(tid).then((team) => {
-				let origin = window.location.origin;
-				let pathname = window.location.pathname;
-				let code = team.joinCode || false;
-				let linkEnd = code ? `&code=${code}` : '';
-				let link = `${origin}${pathname}?team=${tid}${linkEnd}`;
-				vex.dialog.prompt({
-					message: 'Send this link to your teammates:',
-					value: link,
-					callback: () => {}
-				});
-			});
-		});
-
-		teamName.addEventListener('input', (e) => {
-			let name = e.target.innerText;
-			let cleaned = name.split('\n').reduce((agg, val) => {
-				if (val) {
-					return agg + val;
-				} else {
-					return agg;
-				}
-			}, '');
-			//console.log(name, name.split('\n'), cleaned);
-			e.target.innerText = cleaned;
-			cursorManager.setEndOfContenteditable(e.target);
-		});
-
-		teamName.addEventListener('keypress', (e) => {
-			let keyCode = e.keyCode || e.which;
-			if (keyCode == 13) {
-				let name = e.target.innerText;
-				let uid = database.getCurrentUser().uid;
-				database.updateTeamName(tid, uid, name);
-			}
-		});
-
-		saveQuest.addEventListener('click', (e) => {
-			saveQuest.classList.add('is-loading');
-			let uid = database.getCurrentUser().uid;
-			let question = teamQuest.innerText;
-			database.updateQuestion(tid, uid, question).then((done) => {
-				saveQuest.classList.remove('is-loading');
-			}).catch(reportErrorToUser);
-		});
-
-		saveExpect.addEventListener('click', (e) => {
-			saveExpect.classList.add('is-loading');
-			let uid = database.getCurrentUser().uid;
-			let text = teamExpect.innerText;
-			let list = text.split('\n');
-			let popped = list.pop();
-			if (!popped.length < 1) {
-				list.push(popped);
-			}
-			database.updateExpectations(tid, uid, list).then((done) => {
-				saveExpect.classList.remove('is-loading');
-			}).catch(reportErrorToUser);
-		});
-
-		saveUpdate.addEventListener('click', (e) => {
-			saveUpdate.classList.add('is-loading');
-			let uid = database.getCurrentUser().uid;
-			let update = myUpdate.value;
-			database.submitUpdate(tid, uid, update).then((done) => {
-				saveUpdate.classList.remove('is-loading');
-				myUpdate.value = '';
-			}).catch(reportErrorToUser);
-		});
-
+		let checkedPermissions = false;
 		database.onTeamChange(tid, (team, members) => {
-			let uid = database.getCurrentUser().uid;
-			if (uid in members) {
-				if (Object.keys(team).length > 0) {
-					renderTeam(team, members);
-				}
-			} else if (params.code) {
-				vex.dialog.confirm({
-					message: `Do you want to join ${team.name || 'this team'}?`,
-					buttons: [
-						$.extend({}, vex.dialog.buttons.YES, {text: 'Yes'}),
-						$.extend({}, vex.dialog.buttons.NO, {text: 'No'})
-					],
-					callback: (yes) => {
-						if (yes) {
-							database.joinTeam(tid, uid, params.code).then((res) => {
-								if (res.success) {
-									vex.dialog.alert({
-										message: `Congratulations, you just joined ${team.name || 'your new team'}!`
-									});
-									if (Object.keys(team).length > 0) {
-										renderTeam(team, members);
-									}
-								} else {
-									window.location = window.location.origin + '/me.html';
-								}
-							});
-						} else {
-							window.location = window.location.origin + '/me.html';
-						}
-					}
-				});
-			} else if (params.rdr) {
-				if (Object.keys(team).length > 0) {
-					renderTeam(team, members);
-				}
-			} else {
-				window.location = window.location.origin + '/me.html';
+			if (!checkedPermissions) {
+				checkUserPermission(database, team, members);
+				checkedPermissions = true;
 			}
 		}, reportErrorToUser);
+
+		mainCharterTab(user, tid);
+		mainProgressTab(user, tid);
 
 	});
 
 }
 
-function renderTeam(team, members) {
+function mainCharterTab(user, tid) {
+
+	inviteBtn.addEventListener('click', (e) => {
+		database.getTeam(tid).then((team) => {
+			let origin = window.location.origin;
+			let pathname = window.location.pathname;
+			let code = team.joinCode || false;
+			let linkEnd = code ? `&code=${code}` : '';
+			let link = `${origin}${pathname}?team=${tid}${linkEnd}`;
+			vex.dialog.prompt({
+				message: 'Send this link to your teammates:',
+				value: link,
+				callback: () => {}
+			});
+		});
+	});
+
+	teamName.addEventListener('input', (e) => {
+		let name = e.target.innerText;
+		let cleaned = name.split('\n').reduce((agg, val) => {
+			if (val) {
+				return agg + val;
+			} else {
+				return agg;
+			}
+		}, '');
+		//console.log(name, name.split('\n'), cleaned);
+		e.target.innerText = cleaned;
+		cursorManager.setEndOfContenteditable(e.target);
+	});
+
+	teamName.addEventListener('keypress', (e) => {
+		let keyCode = e.keyCode || e.which;
+		if (keyCode == 13) {
+			let name = e.target.innerText;
+			let uid = database.getCurrentUser().uid;
+			database.updateTeamName(tid, uid, name);
+		}
+	});
+
+	saveQuest.addEventListener('click', (e) => {
+		saveQuest.classList.add('is-loading');
+		let uid = database.getCurrentUser().uid;
+		let question = teamQuest.innerText;
+		database.updateQuestion(tid, uid, question).then((done) => {
+			saveQuest.classList.remove('is-loading');
+		}).catch(reportErrorToUser);
+	});
+
+	saveExpect.addEventListener('click', (e) => {
+		saveExpect.classList.add('is-loading');
+		let uid = database.getCurrentUser().uid;
+		let text = teamExpect.innerText;
+		let list = text.split('\n');
+		let popped = list.pop();
+		if (!popped.length < 1) {
+			list.push(popped);
+		}
+		database.updateExpectations(tid, uid, list).then((done) => {
+			saveExpect.classList.remove('is-loading');
+		}).catch(reportErrorToUser);
+	});
+
+	saveUpdate.addEventListener('click', (e) => {
+		saveUpdate.classList.add('is-loading');
+		let uid = database.getCurrentUser().uid;
+		let update = myUpdate.value;
+		database.submitUpdate(tid, uid, update).then((done) => {
+			saveUpdate.classList.remove('is-loading');
+			myUpdate.value = '';
+		}).catch(reportErrorToUser);
+	});
+
+	database.onTeamChange(tid, (team, members) => {
+		if (Object.keys(team).length > 0) {
+				renderTeamCharter(team, members);
+			}
+	}, reportErrorToUser);
+
+}
+
+function mainProgressTab(user, tid) {
+
+	database.onTeamChange(tid, (team, members) => {
+
+		renderProgressUpdates(tid, team, members);
+
+	}, reportErrorToUser);
+
+}
+
+function renderProgressUpdates(tid, team, members) {
+	let updateMap = team.updates || {};
+	
+	let updateList = [];
+
+	for (let uid in updateMap) {
+		let subMap = updateMap[uid];
+		for (let upid in subMap) {
+			let node = subMap[upid];
+			node.uid = uid;
+			node.key = upid;
+			updateList.push(node);
+		}
+	}
+
+	for (let uid in team.members) {
+		let member = team.members[uid];
+		let profile = members[uid];
+		updateList.push({
+			update: `${profile.name} joined the team.`,
+			timestamp: member.joined,
+			uid: uid,
+			key: `${uid}-joined`
+		});
+	}
+
+	updateList.sort((a, b) => {
+		return b.timestamp - a.timestamp;
+	});
+
+	progressUpdates.innerHTML = '';
+
+	if (updateList.length > 0) {
+		updateList.forEach((update) => {
+			let author = members[update.uid];
+			let tile = views.getProgressUpdate({
+				name: author.name,
+				role: team.members[update.uid].role,
+				image: author.image,
+				update: update.update,
+				timestamp: update.timestamp
+			});
+			progressUpdates.appendChild(tile);
+		});
+	} else {
+		let noUpdatesWarning = document.createElement('div');
+		noUpdatesWarning.innerHTML = `<div class="content">
+			<h4>No updates to show :(</h4>
+		</div>`;
+		progressUpdates.appendChild(noUpdatesWarning);
+	}
+}
+
+function renderTeamCharter(team, members) {
 
 	let teamMembers = team.members || {};
 
@@ -214,6 +272,50 @@ function renderTeam(team, members) {
 	}
 }
 
+function checkUserPermission(database, team, members) {
+	/*return new Promise((resolve, reject) => {
+
+	});*/
+	let uid = database.getCurrentUser().uid;
+	if (uid in members) {
+		if (Object.keys(team).length > 0) {
+			renderTeamCharter(team, members);
+		}
+	} else if (params.code) {
+		vex.dialog.confirm({
+			message: `Do you want to join ${team.name || 'this team'}?`,
+			buttons: [
+				$.extend({}, vex.dialog.buttons.YES, {text: 'Yes'}),
+				$.extend({}, vex.dialog.buttons.NO, {text: 'No'})
+			],
+			callback: (yes) => {
+				if (yes) {
+					database.joinTeam(tid, uid, params.code).then((res) => {
+						if (res.success) {
+							vex.dialog.alert({
+								message: `Congratulations, you just joined ${team.name || 'your new team'}!`
+							});
+							if (Object.keys(team).length > 0) {
+								renderTeamCharter(team, members);
+							}
+						} else {
+							window.location = window.location.origin + '/me.html';
+						}
+					});
+				} else {
+					window.location = window.location.origin + '/me.html';
+				}
+			}
+		});
+	} else if (params.rdr) {
+		if (Object.keys(team).length > 0) {
+			renderTeamCharter(team, members);
+		}
+	} else {
+		window.location = window.location.origin + '/me.html';
+	}
+}
+
 function reportErrorToUser(err) {
 	vex.dialog.alert(err + '');
 }
@@ -241,6 +343,23 @@ function fillSrc(className, text) {
 	for (let s = 0; s < spans.length; s++) {
 		spans[s].src = text;
 	}
+}
+
+function onTabClick(tabGroup, tab) {
+	let tabid = tab.dataset.tab;
+	let showTab = document.getElementById(tabid);
+	if (showTab) {
+		for (let j = 0; j < tabGroup.length; j++) {
+			tabGroup[j].classList.remove('is-active');
+		}
+		let otherTabs = document.getElementsByClassName('tabbed-container');
+		for (let i = 0; i < otherTabs.length; i++) {
+			otherTabs[i].style.display = 'none';
+		}
+		showTab.style.display = 'block';
+		tab.classList.add('is-active');
+	}
+
 }
 
 let cancels = 0;
--- src/js/views.js
+++ src/js/views.js
@@ -24,8 +24,8 @@ let Views = () => {
 								</p>
 							</figure>
 							<div class="media-content content">
-								<h3 class="title">${model.name}</h3>
-								<p class="subtitle">${model.role}</p>
+								<h3 class="title is-5">${model.name}</h3>
+								<p class="subtitle is-6">${model.role}</p>
 							</div>
 						</div>
 					</div>
@@ -34,6 +34,47 @@ let Views = () => {
 					<div class="tile is-child">
 						<div class="content">
 							<span class="tag ${tagClass}">${dateFormat}</span> ${model.update}
+						</div>
+					</div>
+				</div>`;
+			let div = document.createElement('div');
+				div.classList.add('tile');
+				div.innerHTML = html;
+			return div;
+		},
+
+		getProgressUpdate: (model) => {
+			let posted = new Date(model.timestamp);
+			let dateFormat = moment(posted).format('M/D h:mm A');
+			let diff = moment(Date.now()).diff(moment(model.timestamp));
+			let daysSinceLastUpdate = moment.duration(diff).asDays();
+			let tagClass = 'is-success';
+			if (daysSinceLastUpdate >= 5) {
+				tagClass = 'is-danger';
+			}
+			else if (daysSinceLastUpdate >= 3) {
+				tagClass = 'is-warning';
+			}
+			let html = `
+				<div class="tile is-parent is-vertical is-4">
+					<div class="tile is-child">
+						<span class="tags has-addons">
+							<span class="tag ${tagClass} is-medium">${dateFormat}</span>
+							<span class="tag is-medium">
+								<span class="image image-tag-rounded is-32x32">
+									<img src="${model.image}">
+								</span>
+								<span class="is-medium">
+									${model.name}
+								</span>
+							</span>
+						</span>
+					</div>
+				</div>
+				<div class="tile is-parent is-vertical is-8">
+					<div class="tile is-child">
+						<div class="content">
+							${model.update}
 						</div>
 					</div>
 				</div>`;
